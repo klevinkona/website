8869bfff9b55054dd4424f7d504a77ba07927e65
Niels Dequeker <niels.dequeker@gmail.com>
2015-01-17 14:40:00 +0100

    Fix deploy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,17 @@
-grunt.loadNpmTasks('grunt-gh-pages');
+'use strict';
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-gh-pages');
 
-grunt.initConfig({
-  'gh-pages': {
-    options: {
-      base: 'app'
-    },
-    src: [
-      '**/*'
-    ]
-  }
-});
+  grunt.initConfig({
+    'gh-pages': {
+      options: {
+        base: 'app'
+      },
+      src: [
+        '**/*'
+      ]
+    }
+  });
 
-grunt.registerTask('deploy', ['gh-pages']);
+  grunt.registerTask('deploy', ['gh-pages']);
+};
